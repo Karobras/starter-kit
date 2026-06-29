70e25bd5794650251bed301eac25a5e894f4ab6b
Karobras <karolinabrasun@gmail.com>
2026-06-29 15:45:44 +0200

    Update formatter.js

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,3 +1,10 @@
+//  Formatter to funkcja, która zmienia sposób wyświetlania danych w widoku (XML)
+//  Czyli:
+// dane w modelu są np. "true"
+// a Ty chcesz pokazać "Aktywny"
+//  Formatter robi tę zamianę 
+// poniższy formater łączy imię i nazwisko -> trzeba się odowłać do niego w controlrze i w xml 
+
 sap.ui.define([], function  () {
     "use strict";
 return {
