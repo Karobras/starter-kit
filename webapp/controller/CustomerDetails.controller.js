2e929b94f7998b654def02c4687f7a8263d5c17b
Karobras <karolinabrasun@gmail.com>
2026-06-29 16:11:53 +0200

    Update CustomerDetails.controller.js

--- webapp/controller/CustomerDetails.controller.js
+++ webapp/controller/CustomerDetails.controller.js
@@ -1,19 +1,20 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "stk/starterkit/model/formatter",
-  "sap/ui/core/Fragment"
+  "sap/ui/core/Fragment" //popup nowe oko z info 
 ], function (Controller, formatter, Fragment ) {
   "use strict";
 
   return Controller.extend("stk.starterkit.controller.CustomerDetails", { Formatter :formatter,
     onInit: function () {
          const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-     oRouter.getRoute("CustomerDetails").attachPatternMatched(this._onPatternMatched, this);
+     oRouter.getRoute("CustomerDetails").attachPatternMatched(this._onPatternMatched, this); //pobranie routera i później wywołanie funkcji 
         },
   _onPatternMatched: function (oEvent) { 
     this.getView().bindElement({ path: "/Customers('" + oEvent.getParameter("arguments").CustomerID + "')", 
-      parameters: { expand: "Orders, Orders/Order_Details , Orders/Employee" }  });                
+      parameters: { expand: "Orders, Orders/Order_Details , Orders/Employee" }  });  //pobranie jednej ścieżki do rekordu i dobranie expand czyli dodatkowych 
          },
+      // przycisk wyjścia obsługa                                                                 
          handleNavButtonPress: function () {
     var oHistory = sap.ui.core.routing.History.getInstance();
     var sPreviousHash = oHistory.getPreviousHash();
@@ -24,6 +25,7 @@ sap.ui.define([
         this.getOwnerComponent().getRouter().navTo("CustomersList", {}, true);
     }
 }, 
+   //kod do formatera - wywołanie popup 
   _onDetailsInfo: function () { 
 var oView = this.getView();
 if(!this.byId("ContactInfoDialog")) {
