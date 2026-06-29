c8e4a4021a8eaebea5998f4f12bf6b7bbd8fed72
Karobras <karolinabrasun@gmail.com>
2026-06-29 15:52:22 +0200

    Enhance Performance.controller.js with comments
    Updated the Performance controller to include comments and improve code readability.

--- webapp/controller/Performance.controller.js
+++ webapp/controller/Performance.controller.js
@@ -1,20 +1,24 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "stk/starterkit/model/formatter",
-  "stk/starterkit/model/calculate"
+  "stk/starterkit/model/formatter", // potrzebne do formatera imienia i nazwika
+  "stk/starterkit/model/calculate" // potrzebne do kalkulacji działa podobnie jak formatter
 ], function (Controller, formatter, calculate) {
   "use strict";
 
-  return Controller.extend("stk.starterkit.controller.Performance", { Formatter :formatter, Calculate :calculate,
+  return Controller.extend("stk.starterkit.controller.Performance", { Formatter :formatter, Calculate :calculate, 
     onInit: function () {
          const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-     oRouter.getRoute("Performance").attachPatternMatched(this._onPatternMatched, this);
+     oRouter.getRoute("Performance").attachPatternMatched(this._onPatternMatched, this); //pobranie danych z routera - zarzadzanie nawigacja miedzy widokami
+      // później podczeniam funkcje ➡️ "Kiedy użytkownik wejdzie na ten route → wykonaj _onPatternMatched"
     
         },
   _onPatternMatched: function (oEvent) { 
     this.getView().bindElement({ path: "/Customers" , 
       parameters: { expand: "Orders, Orders/Order_Details , Orders/Employee" }  });                
-         },
+         }, // ➡️ Bindowanie całego widoku do jednego obiektu pobranie 
+         // expand to pobranie powiązanych danych  
+       
+          // obsługa przycisku back
          handleNavButtonPress: function () {
     var oHistory = sap.ui.core.routing.History.getInstance();
     var sPreviousHash = oHistory.getPreviousHash();
@@ -25,6 +29,7 @@ sap.ui.define([
         this.getOwnerComponent().getRouter().navTo("CustomersList", {}, true);
     }
 },
+// opcjonalnie pobiera API i pokazuje toast
 _oFire: function () { 
   debugger
  fetch("https://api.adviceslip.com/advice")
